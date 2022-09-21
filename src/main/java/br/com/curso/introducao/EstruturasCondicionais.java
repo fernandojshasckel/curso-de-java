0ea6042c5ad5c5bb7ed269528693f3d977fe961c
fernando.hasckel <fernando.hasckel@ciss.com.br>
2022-09-20 22:57:31 -0300

    011 - Estruturas condicionais

--- src/main/java/br/com/curso/introducao/EstruturasCondicionais.java
+++ src/main/java/br/com/curso/introducao/EstruturasCondicionais.java
@@ -11,7 +11,9 @@ public class EstruturasCondicionais {
 
         if (age >= 18){
             System.out.println("Autorizado a comprar bebida alcólica");
-        } 
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
 
         boolean isAuthorized = age >= 18;
 
@@ -21,6 +23,20 @@ public class EstruturasCondicionais {
         
         if (!isAuthorized){
             System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+        int idade = 18;
+        String categoria;
+
+        if (idade < 15){
+            categoria = "Categoria infantil";
+        } else if (idade >= 15 && idade < 18){
+            categoria = "Categoria juvenil";
+        } else if (idade >= 18){
+            categoria = "Categoria adulto";
         }
     }
 }
